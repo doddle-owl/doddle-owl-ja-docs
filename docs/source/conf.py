149c9b7ea98b786f4bb420115e9db648a30b0f7e
Takeshi Morita <takesi.morita@gmail.com>
2016-03-13 21:53:52 +0900

    modify conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -212,6 +212,8 @@ htmlhelp_basename = 'doddle-owl docs'
 
 # -- Options for LaTeX output ---------------------------------------------
 
+latex_docclass = {'manual': 'jsbook'}
+
 latex_elements = {
 # The paper size ('letterpaper' or 'a4paper').
 #'papersize': 'letterpaper',
